a9e3af92f3f3bf916b85d3d575be5ead192ccd5e
Tania Garrido Salido <tanita93@gmail.com>
2016-02-17 12:01:01 -0600

    cambios

--- memorygame.cpp
+++ memorygame.cpp
@@ -19,6 +19,8 @@ int tenthsOfASecond = 0;
 bool paused = true;
 bool started = false;
 bool terminado = false;
+bool desplegar = true;
+bool letras = false;
 int turns = 0;
 char deck[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '0', '1', '2', '3', '4', '5', '6', '7' };
 bool deck_display[16] = { false };
@@ -53,7 +55,10 @@ void randomGlColor3ub(){
 }
 
 void cardColor(int i){
-    if (i % 2 == 0){
+    if (deck_display[i] && !letras) {
+        glColor3ub(255, 255, 255);
+    }
+    else if (i % 2 == 0){
         glColor3ub(150, 51, 184);
     } else{
         glColor3ub(85, 184, 51);
@@ -112,6 +117,7 @@ void displayTurns(){
 
 void displayCards(){
     for (int i = 0; i < 16; i++){
+        letras = false;
         cardColor(i);
         glBegin(GL_POLYGON);
         glVertex2f(i * 100, 500);
@@ -120,6 +126,7 @@ void displayCards(){
         glVertex2f(i * 100, 400);
         glEnd();
         if (deck_display[i]){
+            letras = true;
             cardColor(i+1);
             glPushMatrix();
             glScalef(0.85f, 0.85f, 1);
@@ -131,53 +138,56 @@ void displayCards(){
 }
 
 void displayInstructions(){
-    char start[10] = "I-Iniciar";
-    char pause[10] = "P-Pausa";
-    char reset[15] = "R-Reiniciar";
-    char esc[10] = "Esc-Salir";
-    char tania[40] = "Tania Garrido - A01138941";
-    char abraham[40] = "Abraham Rodríguez - A01195653";
-    
-    glColor3ub(150, 51, 184);
-    glRasterPos2f(350, 150);
-    for(int k = 0; start[k] != '\0'; k++){
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, start[k]);
-    }
-    glRasterPos2f(550, 150);
-    for(int k = 0; pause[k] != '\0'; k++){
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, pause[k]);
-    }
-    glRasterPos2f(750, 150);
-    for(int k = 0; reset[k] != '\0'; k++){
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, reset[k]);
-    }
-    glRasterPos2f(1000, 150);
-    for(int k = 0; esc[k] != '\0'; k++){
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, esc[k]);
-    }
-    glRasterPos2f(100, 110);
-    for(int k = 0; tania[k] != '\0'; k++){
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, tania[k]);
-    }
-    glRasterPos2f(850, 110);
-    for(int k = 0; abraham[k] != '\0'; k++){
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, abraham[k]);
-    }
-    
-    glColor3ub(51, 151, 184);
-    glRasterPos2f(130, 20);
-    for(int k = 0; deck[k] != '\0'; k++){
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, deck[k]);
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ' ');
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ' ');
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ' ');
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ' ');
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ' ');
+    if (desplegar) {
+        char start[10] = "I-Iniciar";
+        char pause[10] = "P-Pausa";
+        char reset[15] = "R-Reiniciar";
+        char esc[10] = "Esc-Salir";
+        char tania[40] = "Tania Garrido - A01138941";
+        char abraham[40] = "Abraham Rodríguez - A01195653";
+        
+        glColor3ub(150, 51, 184);
+        glRasterPos2f(350, 150);
+        for(int k = 0; start[k] != '\0'; k++){
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, start[k]);
+        }
+        glRasterPos2f(550, 150);
+        for(int k = 0; pause[k] != '\0'; k++){
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, pause[k]);
+        }
+        glRasterPos2f(750, 150);
+        for(int k = 0; reset[k] != '\0'; k++){
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, reset[k]);
+        }
+        glRasterPos2f(1000, 150);
+        for(int k = 0; esc[k] != '\0'; k++){
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, esc[k]);
+        }
+        glRasterPos2f(100, 110);
+        for(int k = 0; tania[k] != '\0'; k++){
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, tania[k]);
+        }
+        glRasterPos2f(850, 110);
+        for(int k = 0; abraham[k] != '\0'; k++){
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, abraham[k]);
+        }
+        
+        glColor3ub(51, 151, 184);
+        int x = 70;
+        for(int k = 0; deck[k] != '\0'; k++){
+            glRasterPos2f(x, 405);
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, deck[k]);
+            x+=100;
+        }
     }
 }
 
 void keyboardActions(unsigned char theKey, int mouseX, int mouseY){
     switch (theKey){
+        case 'a':
+        case 'A':
+            desplegar = !desplegar;
+            break;
         case 'i':
         case 'I':
             if (!started){
@@ -229,15 +239,15 @@ void mouseActions(int button, int state, int x, int y){
             }
             else {
                 switch(estados){
-                case 0: //ninguna carta con que aparear
-                    deck_display[cardNumber(x)] = true;
-                    carta1 = cardNumber(x);
-                    estados = 1;//avanzamos el estado
-                    break;
-                case 1: //una baraja expuesta y dimos click en otra
-                    deck_display[cardNumber(x)] = true;
-                    carta2 = cardNumber(x);
-                    estados = 2;
+                    case 0: //ninguna carta con que aparear
+                        deck_display[cardNumber(x)] = true;
+                        carta1 = cardNumber(x);
+                        estados = 1;//avanzamos el estado
+                        break;
+                    case 1: //una baraja expuesta y dimos click en otra
+                        deck_display[cardNumber(x)] = true;
+                        carta2 = cardNumber(x);
+                        estados = 2;
                         if(deck[carta1] == deck[carta2]){
                             if(encontradas == 7){
                                 estados = 3;
@@ -245,26 +255,26 @@ void mouseActions(int button, int state, int x, int y){
                             }
                             encontradas++;
                         }
-
-                    break;
-                case 2:// dos barajas expuestas, dan click en otra
-                    if(!(deck[carta1] == deck[carta2])){//si no son pares regresamos a su esado no expuesto
-                        deck_display[carta1] = false;
-                        deck_display[carta2] = false;
                         
-                        estados = 1;
-                        carta1 = cardNumber(x);
-                        deck_display[cardNumber(x)] = true;
-                        turns++;
-                    }else{//si hay un par expuesto
-                        estados = 1;
-                        carta1 = cardNumber(x);
-                        deck_display[cardNumber(x)] = true;
-                        turns++;
-                    }
-                    break;
+                        break;
+                    case 2:// dos barajas expuestas, dan click en otra
+                        if(!(deck[carta1] == deck[carta2])){//si no son pares regresamos a su esado no expuesto
+                            deck_display[carta1] = false;
+                            deck_display[carta2] = false;
+                            
+                            estados = 1;
+                            carta1 = cardNumber(x);
+                            deck_display[cardNumber(x)] = true;
+                            turns++;
+                        }else{//si hay un par expuesto
+                            estados = 1;
+                            carta1 = cardNumber(x);
+                            deck_display[cardNumber(x)] = true;
+                            turns++;
+                        }
+                        break;
                 }
-            
+                
             }
             
         }
@@ -278,9 +288,10 @@ void game(void){
     glClearColor(67.0/255.0, 183.0/255.0, 222.0/255.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     displayTurns();
-    displayInstructions();
     displayTime();
     displayCards();
+    displayInstructions();
+    
     
     if(estados == 3){
         glColor4f(1,.7,.2,.2);
